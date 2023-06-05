fdaaf65177355fb3eca07110b404afc0dc3afd61
PrakharAIP GSOC <emailkakyakarega>
2023-06-05 12:56:03 +0530

    Added auto_trim module

---
+++ utils/auto_trim/main.py
@@ -0,0 +1,56 @@
+"""
+For microprocessor, the number of blocklists can be too large to be processed by the cpu.
+So this module trims the blocklists to a certain value (using benchmark module)so that the microprocessor can work efficiently.
+Total IP to be added to the firewall = bench_value * trimmed_value_constant
+"""
+
+import json
+import os 
+
+CONFIG_BENCHMARK='configurations/benchmark_config.json'
+CONFIG_AUTOTRIM='configurations/auto_trim_config.json'
+
+
+def trim():
+    # check if auto_trim is enabled
+    with open(CONFIG_AUTOTRIM) as f:
+        data_auto_trim = json.load(f)
+    if data_auto_trim['auto_trim'] == False:
+        return
+    
+    with open(CONFIG_BENCHMARK) as f:
+        data_benchmark = json.load(f)
+    
+    TRIMMED_VALUE_CONSTANT = data_auto_trim['trimmed_value_constant']
+    bench_value = float(data_benchmark['benchmark']['bench_value'])
+
+    TRIMMED_CNT = int(bench_value*TRIMMED_VALUE_CONSTANT)
+    
+    #blocklists folder
+    csvs_folders = './data/Retrieved_blocklists'
+
+    total_files = 0
+    for root, dirs, files in os.walk(csvs_folders):
+        for file in files:
+            if file.endswith('.csv'):
+                csv_dir = os.path.join(root, file)
+                total_files += 1
+
+    TRIMMED_CNT_PER_FILE=int(TRIMMED_CNT/total_files)
+    for root, dirs, files in os.walk(csvs_folders):
+        for file in files:
+            if file.endswith('.csv'):
+                csv_dir = os.path.join(root, file)
+                # Trim csv file
+                with open(csv_dir, 'r') as f:
+                    lines = f.readlines()
+                    trimmed_lines = lines[:TRIMMED_CNT_PER_FILE]
+
+                with open(csv_dir, 'w') as f:
+                    f.writelines(trimmed_lines)
+
+                line_count = len(trimmed_lines)
+                print(f"File: {csv_dir}, Line Count: {line_count}")
+
+if __name__=='__main__':
+    trim()
--- utils/benchmark/main.py
+++ utils/benchmark/main.py
@@ -1,37 +1,43 @@
+"""
+This module is used to benchmark the system. It is used to get the benchmark value of the system and save it in the config file ,if a microprocessor is using this software to protect themself from malicious IP, so this benchmark value can be used to trim the blocklists to a certain value so that the microprocessor can work efficiently.
+"""
+
 import os
 import json
 import subprocess
 
-CONFIG='./configurations/bechmark_config.json'
+CONFIG = './configurations/benchmark_config.json'
 
 
 def benchmarker():
-    #Download Benchmark
+    # Download Benchmark
     print("Downloading Benchmark. Please wait (this might take a while)")
     exit_code = os.system("bash ./utils/benchmark/installer.sh")
     if exit_code != 0:
         print("Failed to install benchmark.")
         exit(1)
     else:
-        print(f"\033[1mBenchmark installed successfully.\033[0m")
+        print(f"\033[1mBenchmarking your device.\033[0m")
 
     # Do benchmark
-    bench_value = subprocess.check_output("sysbench cpu --cpu-max-prime=20000 --time=5 run | grep 'events per second' | awk '   {print $NF}'", shell=True)
-    
-    #create config file if it doesn't exist
-    if not os.path.exists(CONFIG):
-        with open(CONFIG, 'w') as f:
-            json.dump({'benchmark':{}}, f)
+    bench_value = subprocess.check_output(
+        "sysbench cpu --cpu-max-prime=20000 --time=5 run | grep 'events per second' | awk '   {print $NF}'",
+        shell=True)
 
-    # Load the configuration from the .json file
-    with open(CONFIG, 'r') as f:
-        data = json.load(f)
-        
+    # Create config directory if it doesn't exist
+    config_dir = os.path.dirname(CONFIG)
+    os.makedirs(config_dir, exist_ok=True)
+
     # Set benchmark value in config file
-    data['benchmark']['bench_value'] = bench_value.decode('utf-8').strip()
+    data = {'benchmark': {'bench_value': bench_value.decode('utf-8').strip()}}
     with open(CONFIG, 'w') as f:
         json.dump(data, f)
+
+    # Set file permissions for regular user access
+    os.chmod(CONFIG, 0o644)
+
     print(f"\033[1mBenchmark value set to: ", bench_value.decode('utf-8').strip())
+
 
 if __name__ == "__main__":
     benchmarker()
