ed01bb5a51fb5be05b81e4633fcab10b3c4e0b62
PrakharAIP GSOC <emailkakyakarega>
2023-06-05 12:21:02 +0530

    changed benchmark to check if tool already installed

--- utils/benchmark/installer.sh
+++ utils/benchmark/installer.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-# check sudo permission
-if [ "$EUID" -ne 0 ]
-then 
-    echo -e "\e[91mPlease run as root\e[0m"
-    exit 1
-fi
-
-# install benchmark
-if sudo apt-get install sysbench -y; then
-    echo "Installation successful"
+# Check if sysbench is already installed
+if ! command -v sysbench &>/dev/null; then
+    # Check sudo permission
+    if [ "$EUID" -ne 0 ]; then 
+        echo -e "\e[91mPlease run as root\e[0m"
+        exit 1
+    fi
+    
+    # Install benchmark
+    if sudo apt-get install sysbench -y; then
+        echo "Installation successful"
+    else
+        echo "Installation failed"
+        exit 1
+    fi
 else
-    echo "Installation failed"
-    exit 1
+    echo "Benchmark already installed. Skipping installation."
 fi
